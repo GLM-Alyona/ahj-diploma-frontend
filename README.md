b407360e07c353e572a892dedb7b0c1951693478
Alyona <glmalyona@gmail.com>
2023-09-28 22:53:23 +0300

    1 commit

---
+++ README.md
@@ -0,0 +1,4 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/no47jv0nxea4pvyf?svg=true)](https://ci.appveyor.com/project/GLM-Alyona/ahj-diploma-frontend)
+
+# Chaos Organizer
+
---
+++ src/js/utils/textfilter.js
@@ -0,0 +1,7 @@
+// фильтрация вводимого сообщение на наличие ссылок
+
+export default function (str) {
+  let re = /([^\"=]{2}|^)((https?|ftp):\/\/\S+[^\s.,> )\];'\"!?])/;
+  let subst = '$1<a href="$2" target="_blank">$2</a>';
+  return str.replace(re, subst);
+}
